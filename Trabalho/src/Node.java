d16ba715a37050d03231e01e926d3ea476c8a6dd
Mateus Moreira <mateusmoreirafernandes75@gmail.com>
2025-09-09 13:59:48 -0300

    CriaçãoClasseNode

---
+++ Trabalho/src/Node.java
@@ -0,0 +1,21 @@
+public class Node {
+    private Node no;
+    private Node next;
+    public Node(Node no, Node next) {
+        this.no = no;
+        this.next = next;
+    }
+    public Node getNo() {
+        return no;
+    }
+    public void setNo(Node no) {
+        this.no = no;
+    }
+    public Node getNext() {
+        return next;
+    }
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+}
